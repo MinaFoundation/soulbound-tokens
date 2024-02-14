8d8a5d3b2028fb716f94846102cc545468103471
Philipp Kant <philipp.kant@minafoundation.com>
2024-02-14 13:32:40 +0100

    Add an interface for querying a `PublicKey` for an associated sbt.

--- src/interfaces/SBTService.ts
+++ src/interfaces/SBTService.ts
@@ -1,4 +1,4 @@
-import { MerkleMapWitness, Signature } from "o1js";
+import { MerkleMapWitness, PublicKey, Signature } from "o1js";
 import { SoulboundMetadata, SoulboundRequest } from "../SoulboundMetadata";
 
 interface SBTService {
@@ -7,4 +7,8 @@ interface SBTService {
     verify(metadata: SoulboundMetadata): Promise<MerkleMapWitness>
 }
 
-export { SBTService }
+interface SBTQuery {
+    getSoulboundToken(owner: PublicKey): SoulboundMetadata | undefined
+}
+
+export { SBTService, SBTQuery }
